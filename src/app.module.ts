a5696137540decab83e1e69358642873f8af2737
Hannah <ghdcoalss33@gmail.com>
2023-04-14 16:38:36 +0900

    fix(module): graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { ProductImageModule } from './product-image/product-image.module';
-import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloFederationDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -26,7 +26,7 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
       synchronize: process.env.SYNCHRONIZE === 'true',
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
+      driver: ApolloFederationDriver,
       autoSchemaFile: true,
     }),
     ProductModule,
